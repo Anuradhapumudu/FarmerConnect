64ba7657483dbb41b81036ff5e4477ba8b1c9a92
MenujaAlwis <menujaalwis@gmail.com>
2025-09-02 06:17:57 +0530

    Add Login Form

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -16,6 +16,8 @@
                     $formType = 'officer';
                 } elseif (isset($_POST['brn'])) {
                     $formType = 'seller';
+                } elseif (isset($_POST['admin_id'])) {
+                    $formType = 'admin';
                 }
 
                 // Input data
@@ -30,6 +32,7 @@
                     'confirm_password' => trim($_POST['confirm_password']),
                     'officerID' => '',
                     'brn' => '',
+                    
 
                     'first_name_error' => '',
                     'last_name_error' => '',
@@ -169,6 +172,13 @@
                 if (empty($data['first_name_error']) && empty($data['last_name_error']) && empty($data['email_error']) && empty($data['password_error']) && empty($data['confirm_password_error'])) {
                     // Hash password
                     $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+                    // Set ApprovalStatus according to user type
+                    if ($data['form_type'] === 'seller') {
+                        $data['approval_status'] = 'Pending';
+                    } else {
+                        $data['approval_status'] = NULL;
+                    }
+
                     // Register user
                     if ($this->userModel->register($data)) {
                         // Redirect to the login page
@@ -205,6 +215,217 @@
                 ];
                 // Load the view with the initial data
                 $this->view('users/v_register', $data);
+            }
+        }
+
+
+
+        // Login logic here
+        public function login() {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                // Process login form
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                // Determine which form was submitted
+                if (isset($_POST['officer_id'])) {
+                    $formType = 'officer';
+                } elseif (isset($_POST['seller_id'])) {
+                    $formType = 'seller';
+                } elseif (isset($_POST['admin_id'])) {
+                    $formType = 'admin';
+                } else {
+                    $formType = 'farmer';
+                }
+
+                // Input data
+                if ($formType === 'farmer') { // Farmer
+                    $data = [
+                        'form_type' => $formType,
+                        'nic' => trim($_POST['nic']),
+                        'password' => trim($_POST['password']),
+                        'officer_id' => '',
+                        'seller_id' => '',
+                        'admin_id' => '',
+                        'farmer_nic_error' => '',
+                        'password_error' => '',
+                        'officer_id_error' => '',
+                        'seller_id_error' => '',
+                        'admin_id_error' => ''
+                    ];
+                    // Validate farmer inputs
+                    if (empty($data['nic'])) {
+                        $data['farmer_nic_error'] = 'Please enter your NIC';
+                    } else {
+                        if (!$this->userModel->findUserByNic($data['nic'])) {
+                            $data['farmer_nic_error'] = 'No user found';
+                        }
+                    }
+                    if (empty($data['password'])) {
+                        $data['password_error'] = 'Please enter your password';
+                    }
+                } elseif ($formType === 'officer') { // Officer
+                    $data = [
+                        'form_type' => $formType,
+                        'officer_id' => trim($_POST['officer_id']),
+                        'password' => trim($_POST['password']),
+                        'nic' => '',
+                        'seller_id' => '',
+                        'admin_id' => '',
+                        'farmer_nic_error' => '',
+                        'password_error' => '',
+                        'officer_id_error' => '',
+                        'seller_id_error' => '',
+                        'admin_id_error' => ''
+                    ];
+                    // Validate officer inputs
+                    if (empty($data['officer_id'])) {
+                        $data['officer_id_error'] = 'Please enter your Officer ID';
+                    } else {
+                        if (!$this->userModel->findUserByOfficerId($data['officer_id'])) {
+                            $data['officer_id_error'] = 'No user found';
+                        }
+                    }
+                    if (empty($data['password'])) {
+                        $data['password_error'] = 'Please enter your password';
+                    }
+                } elseif ($formType === 'seller') { // Seller
+                    $data = [
+                        'form_type' => $formType,
+                        'seller_id' => trim($_POST['seller_id']),
+                        'password' => trim($_POST['password']),
+                        'nic' => '',
+                        'officer_id' => '',
+                        'admin_id' => '',
+                        'farmer_nic_error' => '',
+                        'password_error' => '',
+                        'officer_id_error' => '',
+                        'seller_id_error' => '',
+                        'admin_id_error' => ''
+                    ];
+                    // Validate seller inputs
+                    if (empty($data['seller_id'])) {
+                        $data['seller_id_error'] = 'Please enter your Seller ID';
+                    } else {
+                        if (!$this->userModel->findUserBySellerId($data['seller_id'])) {
+                            $data['seller_id_error'] = 'No user found';
+                        }
+                    }
+                    if (empty($data['password'])) {
+                        $data['password_error'] = 'Please enter your password';
+                    }
+                } else { // Admin
+                    $data = [
+                        'form_type' => $formType,
+                        'admin_id' => trim($_POST['admin_id']),
+                        'password' => trim($_POST['password']),
+                        'nic' => '',
+                        'officer_id' => '',
+                        'seller_id' => '',
+                        'farmer_nic_error' => '',
+                        'password_error' => '',
+                        'officer_id_error' => '',
+                        'seller_id_error' => '',
+                        'admin_id_error' => ''
+                    ];
+                    // Validate admin ID
+                    if (empty($data['admin_id'])) {
+                        $data['admin_id_error'] = 'Please enter your Admin ID';
+                    } else {
+                        if (!$this->userModel->findUserByAdminId($data['admin_id'])) {
+                            $data['admin_id_error'] = 'No user found';
+                        }
+                    }
+                    if (empty($data['password'])) {
+                        $data['password_error'] = 'Please enter your password';
+                    }
+                }
+
+                // Check for user
+                switch ($formType) {
+                    case 'farmer':
+                        $data['username'] = $data['nic'];
+                        if (empty($data['farmer_nic_error']) && empty($data['password_error'])) {
+                            $loggedUser = $this->userModel->login($formType ,$data['username'], $data['password']);
+                            if ($loggedUser) {
+                                // Create session,
+                                die('Success');
+                            } else {
+                                $data['password_error'] = 'Password incorrect';
+                                // Load the view with errors
+                                $this->view('users/v_login', $data);
+                            }
+                        } else {
+                            // Load the view with errors
+                            $this->view('users/v_login', $data);
+                        }
+                        break;
+                    case 'officer':
+                        $data['username'] = $data['officer_id'];
+                        if (empty($data['officer_id_error']) && empty($data['password_error'])) {
+                            $loggedUser = $this->userModel->login($formType ,$data['username'], $data['password']);
+                            if ($loggedUser) {
+                                // Create session,
+                            } else {
+                                $data['password_error'] = 'Password incorrect';
+                                // Load the view with errors
+                                $this->view('users/v_login', $data);
+                            }
+                        } else {
+                            // Load the view with errors
+                            $this->view('users/v_login', $data);
+                        }
+                        break;
+                    case 'seller':
+                        $data['username'] = $data['seller_id'];
+                        if (empty($data['seller_id_error']) && empty($data['password_error'])) {
+                            $loggedUser = $this->userModel->login($formType ,$data['username'], $data['password']);
+                            if ($loggedUser) {
+                                // Create session,
+                            } else {
+                                $data['password_error'] = 'Password incorrect';
+                                // Load the view with errors
+                                $this->view('users/v_login', $data);
+                            }
+                        } else {
+                            // Load the view with errors
+                            $this->view('users/v_login', $data);
+                        }
+                        break;
+                    case 'admin':
+                        $data['username'] = $data['admin_id'];
+                        if (empty($data['admin_id_error']) && empty($data['password_error'])) {
+                            $loggedUser = $this->userModel->login($formType ,$data['username'], $data['password']);
+                            if ($loggedUser) {
+                                // Create session,
+                            } else {
+                                $data['password_error'] = 'Password incorrect';
+                                // Load the view with errors
+                                $this->view('users/v_login', $data);
+                            }
+                        } else {
+                            // Load the view with errors
+                            $this->view('users/v_login', $data);
+                        }
+                        break;
+                }
+            } else {
+                // Initial form
+                $data = [
+                    'form_type' => 'farmer', // Default form type
+                    'nic' => '',
+                    'officer_id' => '',
+                    'seller_id' => '',
+                    'admin_id' => '',
+                    'password' => '',
+                    'farmer_nic_error' => '',
+                    'officer_id_error' => '',
+                    'seller_id_error' => '',
+                    'admin_id_error' => '',
+                    'password_error' => ''
+                ];
+                // Load the view with the initial data
+                $this->view('users/v_login', $data);
             }
         }
     }
